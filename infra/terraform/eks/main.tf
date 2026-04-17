46a1f705976477f3818838a0607bd6dd6680b745
Saiteja Bodapati <saitejabodapati925@gmail.com>
2026-04-17 16:51:32 -0500

    update AWS auth for EKS workflow

--- infra/terraform/eks/main.tf
+++ infra/terraform/eks/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.5.7"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.39"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 data "aws_availability_zones" "available" {
   state = "available"
 }
